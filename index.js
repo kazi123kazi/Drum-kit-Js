91644e72e5d87a75ebeb759198bdb6f6cc453ad6
KAZI SAHARIAR RAHI <srahi00000@gmail.com>
2020-04-08 09:06:59 +0530

    uploaded files from local repo to remote repo

---
+++ index.js
@@ -0,0 +1,63 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        
+        var drumName=this.innerHTML;
+        buttonAnimation(drumName);
+        makeSound(drumName);
+    
+       
+        
+        } );  
+}
+document.addEventListener("keypress",function(event){
+        buttonAnimation(event.key);
+        makeSound(event.key);
+  
+});
+
+function makeSound(key) {
+        switch (key) {
+                case "w" :
+                    var audio =new Audio('sounds/tom-1.mp3');
+                    audio.play();
+                    break;
+                 case "a" :
+                        var audio =new Audio('sounds/tom-2.mp3');
+                        audio.play();
+                        break;
+                case "s" :
+                        var audio =new Audio('sounds/tom-3.mp3');
+                        audio.play();
+                        break;
+                case "d" :
+                        var audio =new Audio('sounds/tom-4.mp3');
+                        audio.play();
+                        break;
+                case "j" :
+                        var audio =new Audio('sounds/snare.mp3');
+                         audio.play();
+                         break;   
+                 case "k" :
+                     var audio =new Audio('sounds/crash.mp3');
+                        audio.play();
+                        break; 
+                case "l" :
+                        var audio =new Audio('sounds/kick-bass.mp3');
+                          audio.play();
+                          break;      
+    
+    
+            }
+}
+function buttonAnimation(currentKey) {
+       var active= document.querySelector("."+currentKey);
+       active.classList.add("pressed");
+       setTimeout(function() {
+               active.classList.remove("pressed");
+       } ,100) ;
+      
+     
+
+}
+ 
